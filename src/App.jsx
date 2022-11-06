e61c8ec414855c8bbd4fbe6dcaa965bda4d9a369
satish-313 <pradhansatish53@gmail.com>
2022-11-06 23:22:39 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
 import { AiFillGoogleCircle } from "react-icons/ai";
+import { useRef } from "react";
 
 function App() {
-  const onResponse = async(res) => {
-    console.log(res)
-  }
+  const googleSignInButton = useRef(null);
+
+  const onResponse = async (res) => {
+    console.log("res ", res);
+  };
 
   const fire = () => {
     window.google.accounts.id.initialize({
-      client_id: "368662961806-c8f2hsectd7urdaiq24dr4n86scbda19.apps.googleusercontent.com",
+      client_id:
+        "368662961806-c8f2hsectd7urdaiq24dr4n86scbda19.apps.googleusercontent.com",
       callback: onResponse,
-      auto_select: false
-    })
-    window.google.accounts.id.prompt()
-  }
+    });
+    window.google.accounts.id.renderButton(googleSignInButton.current, {
+      type: "icon",
+      shape: "circle",
+    });
+  };
 
   return (
     <div className="container">
       <h1>hello world</h1>
       <div>
         <button onClick={fire}>
-          <AiFillGoogleCircle />
+          <div ref={googleSignInButton}>
+            <img src="/google-icon.png" alt="" />
+          </div>
         </button>
       </div>
     </div>
